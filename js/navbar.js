a43ce226cae8bf276f5100d5b30fbbdbded1c8a3
Justin <justin.yang8499@gmail.com>
2022-01-22 01:59:32 -0500

    asf
    asf

--- js/navbar.js
+++ js/navbar.js
@@ -4,7 +4,11 @@ navbar.innerHTML = `
   <div class="w-full md-padding-x">
     <div class="flex items-start container mx-auto py-5">
       <div class="flex-1">
-        AA
+        <div class="menu-toggle" :class="showMenu ? 'is-active' : ''" id="mobile-menu" @click="toggleMenu()">
+          <span class="bar" :style="[showMenu ? {'background-color':'#000'} : {'':''}]"></span>
+          <span class="bar" :style="[showMenu ? {'background-color':'#000'} : {'':''}]"></span>
+          <span class="bar" :style="[showMenu ? {'background-color':'#000'} : {'':''}]"></span>
+        </div>
       </div>
       <div>
         <img class="bluebird-logo" src="assets/logos/logo.png" style="object-fit: contain;" />
--- js/vue.js
+++ js/vue.js
@@ -3,14 +3,20 @@ document.querySelectorAll('[data-type="vue"]').forEach(dom => {
 	vueApp[dom.id] = new Vue ({
 		el: '#' + appId,
 		data:{
-			
+			showMenu: false,
+			currentTab: appId
 		},
 		// delimiters:['%{', '}'],
 		watch:{
 
 		},
 		methods:{
-			
+			toggleMenu(){
+				this.showMenu = !this.showMenu
+			},
+			navigateHome(){
+				location.href='/'
+			}
 		},
 		mounted(){
 			
