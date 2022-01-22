1d818d3ca70f008a1baaba3f49e87523741fbf19
Justin <justin.yang8499@gmail.com>
2022-01-22 00:49:34 -0500

    asf
    asf

--- js/navbar.js
+++ js/navbar.js
@@ -2,13 +2,16 @@ const navbar = document.getElementById('nav-bar');
 navbar.innerHTML = `
 <nav>
   <div class="w-full md-padding-x">
-    <div class="container mx-auto py-5">
-      <div class="xs-text">XS TEXT</div>
-      <div class="sm-text">SM TEXT</div>
-      <div class="md-text">MD TEXT</div>
-      <div class="lg-text">LG TEXT</div>
-      <div class="xl-text">XL TEXT</div>
-      <div class="xxl-text">XXL TEXT</div>
+    <div class="flex items-start container mx-auto py-5">
+      <div class="flex-1">
+        AA
+      </div>
+      <div>
+        <img class="bluebird-logo" src="assets/logos/logo.png" style="object-fit: contain;" />
+      </div>
+      <div class="flex-1">
+        AA
+      </div>
     </div>
   </div>
 </nav>
