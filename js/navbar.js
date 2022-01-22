8f48d21ef750909e1126fca308a6b2d375f77344
Justin <justin.yang8499@gmail.com>
2022-01-22 00:21:28 -0500

    Styles and Logos

--- js/navbar.js
+++ js/navbar.js
@@ -1,30 +1,15 @@
 const navbar = document.getElementById('nav-bar');
 navbar.innerHTML = `
-<div class="nav-wrapper md:container md:mx-auto" style="background-color: transparent;">
-  <nav class="navbar container-padding">
-    <img v-if="!showMenu" @click="navigateHome()" src="assets/logo-white.png" alt="Company Logo" style="cursor: pointer;">
-    <div class="menu-toggle" :class="showMenu ? 'is-active' : ''" id="mobile-menu" @click="toggleMenu()">
-      <span class="bar" :style="[showMenu ? {'background-color':'#000'} : {'':''}]"></span>
-      <span class="bar" :style="[showMenu ? {'background-color':'#000'} : {'':''}]"></span>
-      <span class="bar" :style="[showMenu ? {'background-color':'#000'} : {'':''}]"></span>
+<nav>
+  <div class="w-full md-padding-x">
+    <div class="container mx-auto py-5">
+      <div class="xs-text">XS TEXT</div>
+      <div class="sm-text">SM TEXT</div>
+      <div class="md-text">MD TEXT</div>
+      <div class="lg-text">LG TEXT</div>
+      <div class="xl-text">XL TEXT</div>
+      <div class="xxl-text">XXL TEXT</div>
     </div>
-    <ul class="sm-font-size bodyFont nav no-search" :class="showMenu ? 'mobile-nav' : ''" style="line-height: 1.2;">
-      <div v-if="showMenu" class="navbar container-padding">
-        <img @click="navigateHome()" src="assets/logo-black.png" alt="Company Logo" style="cursor: pointer;">
-        <div class="menu-toggle" :class="showMenu ? 'is-active' : ''" id="mobile-menu" @click="toggleMenu()">
-          <span class="bar" :style="[showMenu ? {'background-color':'#000'} : {'':''}]"></span>
-          <span class="bar" :style="[showMenu ? {'background-color':'#000'} : {'':''}]"></span>
-          <span class="bar" :style="[showMenu ? {'background-color':'#000'} : {'':''}]"></span>
-        </div>
-      </div>
-      <li class="nav-item"><a href="/" :style="[currentTab == 'home' ? {'color':'var(--darkblue)'} : {'':''}]">Home</a></li>
-      <li class="nav-item"><a href="/property-management" :style="[currentTab == 'property-management' ? {'color':'var(--darkblue)'} : {'':''}]">Property Management</a></li>
-      <li class="nav-item"><a href="/tenant-placement" :style="[currentTab == 'tenant-placement' ? {'color':'var(--darkblue)'} : {'':''}]">Tenant Placement</a></li>
-      <li class="nav-item"><a href="/tenant-rental-application" :style="[currentTab == 'tenant-rental-application' ? {'color':'var(--darkblue)'} : {'':''}]">Tenant Rental Application</a></li>
-      <li class="nav-item"><a href="/faq" :style="[currentTab == 'faq' ? {'color':'var(--darkblue)'} : {'':''}]">FAQ</a></li>
-      <li class="nav-item"><a href="/contact" :style="[currentTab == 'contact' ? {'color':'var(--darkblue)'} : {'':''}]">Contact Us</a></li>
-      <li class="nav-item"><a href="/properties" :style="[currentTab == 'properties' ? {'color':'var(--darkblue)'} : {'':''}]">Properties</a></li>
-    </ul>
-  </nav>
   </div>
+</nav>
 `;
