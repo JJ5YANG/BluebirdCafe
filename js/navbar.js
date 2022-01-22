c98a8bd045df5e996f4186e816b028502f248380
Justin <justin.yang8499@gmail.com>
2022-01-22 02:37:22 -0500

    asf
    asf

--- js/navbar.js
+++ js/navbar.js
@@ -9,6 +9,15 @@ navbar.innerHTML = `
           <span class="bar" :style="[showMenu ? {'background-color':'#000'} : {'':''}]"></span>
           <span class="bar" :style="[showMenu ? {'background-color':'#000'} : {'':''}]"></span>
         </div>
+        <div class="nav-menu w-full h-screen absolute left-0 top-0" :class="showMenu ? 'mobile-nav' : ''" style="background-color: white; z-index: 3;">
+          <div class="flex w-full justify-end py-5 md-padding-x">
+            <div class="menu-toggle" :class="showMenu ? 'is-active' : ''" id="mobile-menu" @click="toggleMenu()">
+              <span class="bar" :style="[showMenu ? {'background-color':'#000'} : {'':''}]"></span>
+              <span class="bar" :style="[showMenu ? {'background-color':'#000'} : {'':''}]"></span>
+              <span class="bar" :style="[showMenu ? {'background-color':'#000'} : {'':''}]"></span>
+            </div>
+          </div>
+        </div>
       </div>
       <div>
         <img class="bluebird-logo" src="assets/logos/logo.png" style="object-fit: contain;" />
