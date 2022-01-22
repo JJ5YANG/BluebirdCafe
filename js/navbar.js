36be8bcad7a0907730a624aa000273d8850513c9
Justin <justin.yang8499@gmail.com>
2022-01-22 02:08:06 -0500

    asf
    asf

--- js/navbar.js
+++ js/navbar.js
@@ -3,7 +3,7 @@ navbar.innerHTML = `
 <nav>
   <div class="w-full md-padding-x">
     <div class="flex items-start container mx-auto py-5">
-      <div class="flex-1">
+      <div class="flex-1 flex justify-start">
         <div class="menu-toggle" :class="showMenu ? 'is-active' : ''" id="mobile-menu" @click="toggleMenu()">
           <span class="bar" :style="[showMenu ? {'background-color':'#000'} : {'':''}]"></span>
           <span class="bar" :style="[showMenu ? {'background-color':'#000'} : {'':''}]"></span>
@@ -14,7 +14,7 @@ navbar.innerHTML = `
         <img class="bluebird-logo" src="assets/logos/logo.png" style="object-fit: contain;" />
       </div>
       <div class="flex-1">
-        AA
+        
       </div>
     </div>
   </div>
