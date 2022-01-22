61c61fdd80d7c252999e5f0510f69b412056bc6d
Justin <justin.yang8499@gmail.com>
2022-01-22 14:43:46 -0500

    asf
    asf

--- js/navbar-simple.js
+++ js/navbar-simple.js
@@ -2,19 +2,15 @@ const navbar = document.getElementById('nav-bar');
 navbar.innerHTML = `
 <nav>
   <div class="w-full md-padding-x">
-    <div class="flex items-start container mx-auto py-5">
+    <div class="flex items-center container mx-auto py-5">
       <div class="flex-1 flex justify-start">
-        <div class="menu-toggle" :class="showMenu ? 'is-active' : ''" id="mobile-menu" @click="toggleMenu()">
-          <span class="bar" :style="[showMenu ? {'background-color':'#000'} : {'':''}]"></span>
-          <span class="bar" :style="[showMenu ? {'background-color':'#000'} : {'':''}]"></span>
-          <span class="bar" :style="[showMenu ? {'background-color':'#000'} : {'':''}]"></span>
-        </div>
+        <img @click="navigateHome()" class="bluebird-logo" src="assets/logos/logo-straight.png" style="object-fit: contain;" />
         <div class="nav-menu w-full h-screen absolute left-0 top-0" :class="showMenu ? 'mobile-nav' : ''" style="background-color: white; z-index: 3;">
           <div class="flex w-full justify-end py-5 md-padding-x">
             <div class="menu-toggle" :class="showMenu ? 'is-active' : ''" id="mobile-menu" @click="toggleMenu()">
-              <span class="bar" :style="[showMenu ? {'background-color':'#000'} : {'':''}]"></span>
-              <span class="bar" :style="[showMenu ? {'background-color':'#000'} : {'':''}]"></span>
-              <span class="bar" :style="[showMenu ? {'background-color':'#000'} : {'':''}]"></span>
+              <span class="bar" style="background-color: #000;"></span>
+              <span class="bar" style="background-color: #000;"></span>
+              <span class="bar" style="background-color: #000;"></span>
             </div>
           </div>
           <div class="w-full text-center flex justify-center">
@@ -43,12 +39,31 @@ navbar.innerHTML = `
           </div>
         </div>
       </div>
-      <div>
-        <img @click="navigateHome()" class="bluebird-logo" src="assets/logos/logo.png" style="object-fit: contain;" />
-      </div>
-      <div class="flex-1 flex justify-end">
-        <a href="https://www.instagram.com/" class="social-icons" target="_blank"><i class="fab fa-instagram"></i></a>
-        <a href="https://www.facebook.com/" class="social-icons" target="_blank"><i class="fab fa-facebook-square"></i></a>
+      <div class="flex-1 flex justify-end items-center">
+        <div class="menu-toggle lg:hidden" :class="showMenu ? 'is-active' : ''" id="mobile-menu" @click="toggleMenu()">
+          <span class="bar" style="background-color: #000;"></span>
+          <span class="bar" style="background-color: #000;"></span>
+          <span class="bar" style="background-color: #000;"></span>
+        </div>
+        <div class="hidden lg:flex items-center font-bold sm-text">
+          <div class="nav-item-desktop">
+            <a href="/index.html" :style="[currentTab == 'vue-home' ? {'color':'var(--green)'} : {'':''}]">
+              HOME
+            </a>
+          </div>
+          <div class="nav-item-desktop">
+            <a href="/contact.html" :style="[currentTab == 'vue-contact' ? {'color':'var(--green)'} : {'':''}]">
+              CONTACT US
+            </a>
+          </div>
+          <div class="nav-item-desktop">
+            <a href="/menu.html" :style="[currentTab == 'vue-menu' ? {'color':'var(--green)'} : {'':''}]">
+              OUR MENU
+            </a>
+          </div>
+        </div>
+        <a href="https://www.instagram.com/" class="social-icons hidden lg:block" target="_blank"><i class="fab fa-instagram" style="color: var(--black);"></i></a>
+        <a href="https://www.facebook.com/" class="social-icons hidden lg:block" target="_blank"><i class="fab fa-facebook-square" style="color: var(--black);"></i></a>
       </div>
     </div>
   </div>
